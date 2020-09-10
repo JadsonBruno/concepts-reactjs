26e2a1d5be4e58b05ea790ba962c9e926a1c4346
jadsom bruno <jadsombruno2016@gmail.com>
2020-09-10 20:12:01 -0300

    Get all repositories from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import {useState} from 'react';
+import {useEffect} from 'react';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  /**
+   * I get all repositories from api.
+   */
+  async function getRepositories ()
+  {
+    const response = await api.get('/repositories');
+    setRepositories(response.data);
+  }
+
+  useEffect(() => {
+    getRepositories();
+  }, []);
+
   async function handleAddRepository() {
     // TODO
   }
@@ -14,13 +32,15 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+             Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
